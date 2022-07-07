7719d045806580078fcaf4b3cb29f05d31629eec
Facundo <catuno_rolon@hotmail.com>
2022-07-07 19:48:25 -0300

    Primera pre entrega proyecto final

--- js/index.js
+++ js/index.js
@@ -1,17 +1,4 @@
 
-let notaMatematica = 0;
-let notaLiteratura = 0;
-let notaGeografia = 0;
-let nombreAlumno = "";
-let cantNotasMatematica = 0;
-let cantNotasGeografica = 0;
-let cantNotasLiteratura = 0;
-let opcion = 0;
-
-
-//////////////////////////////////////////////
-
-
 //declaro dos objetos de socio
 let socio1 = { nombre: "Facundo Alvarez", edad: 25, localidad: "Rolon", dni: 34475458 };
 let socio2 = { nombre: "Laura Perez", edad: 60, localidad: "Santa Rosa", dni: 14285470 };
@@ -30,38 +17,101 @@ function Socio(dni, nombre, edad, localidad) {
 
 
 function agregarSocio() {
-    let dniSocio = agregarDato("Ingrese el DNI del socio: ");
-    let nombreSocio = agregarDato("Ingrese el nombre del Socio: ");
-    let edadSocio = parseInt(agregarDato("Ingrese la edad del Socio: "));
-    let localidadSocio = agregarDato("Ingrese la localidad del Socio: ");
+    let dniSocio = parseInt(document.getElementById("dniSocioAgregar").value);
+    let nombreSocio = document.getElementById("nombreSocioAgregar").value;
+    let edadSocio = parseInt(document.getElementById("edadSocioAgregar").value);
+    let localidadSocio = document.getElementById("localidadSocioAgregar").value;
 
     //declaramos un variable donde cargar el objeto socio creado arriba
     let socioTemporal = new Socio(dniSocio, nombreSocio, edadSocio, localidadSocio);
-
-    //funcion para agregar dato a cada variable por el metodo prompt
-    function agregarDato(mensaje) {
-        let dato = prompt(mensaje);
-        return dato;
-    }
-    //comprovamos que los campos no esten vacios
-    if (dniSocio == "" || nombreSocio == "" || edadSocio == "" || localidadSocio == "") {
-        alert("Debes completar todos los campos")
+    
+    //verificar que el dni no este cargado en el array previamente
+   
+    const resultado = socios.find((el) => el.dni === dniSocio);
+    if (resultado != null) {
+        alert("Ya existe un socio con este numero de documento");
     } else {
-        //cargamos el objetos socioTemporal al array de socios
-        socios.push(socioTemporal);
+        //comprovamos que los campos no esten vacios
+        if (dniSocio == "" || nombreSocio == "" || edadSocio == "" || localidadSocio == "") {
+            alert("Debes completar todos los campos")
+        } else {
+            //cargamos el objetos socioTemporal al array de socios
+            socios.push(socioTemporal);
+            alert("Se agrego a " + nombreSocio + " exitosamente");
+        }
     }
     return socios;
 }
 
+//mostrar array por LI
+function agregarElementos() {
+    var lista = document.getElementById("ulListado");
+    socios.forEach(function (data, index) {
+        var linew = document.createElement("li");
+        var contenido = document.createTextNode("DNI: " + data.dni + ' | Nombre: ' + data.nombre + " | Edad: " + data.edad + " | Localidad: " + data.localidad);
+        lista.appendChild(linew);
+        linew.appendChild(contenido);
+
+    })
+}
+
+//mostrar array por consola
 function listarSocios() {
     //metodo forof para poder listar el contenido de un array
     for (const socio of socios) {
         console.log(socio.dni, "|", socio.nombre, "|", socio.edad, "|", socio.localidad);
     }
-
-
 }
 
+//buscar socio por dni o nombre
+function buscarSocio() {
+    let dni = parseInt(document.getElementById("documento").value);
+    const resultado = socios.find((el) => el.dni === dni);
+    if (resultado == null) {
+        alert("Socio/s inexistente/s");
+    } else {
+        document.getElementById("nombreSocio").value = resultado.nombre;
+        document.getElementById("localidadSocio").value = resultado.localidad;
+        document.getElementById("edadSocio").value = resultado.edad;
+    }
+    return resultado;
+}
+
+function eliminarSocio() {
+    let dni = parseInt(document.getElementById("documento").value);
+    const resultado = socios.find((el) => el.dni === dni);
+    let position = socios.indexOf(resultado);
+
+    let bandera = document.getElementById("nombreSocio").value;
+    if (bandera == "") {
+        alert("No se ha cargado ningun socio");
+    } else {
+        socios.splice(position, 1);
+        alert("Se elimino a "+ bandera + " exitosamente");
+    }
+    return socios;
+}
+//limpia el frmulario de busqueda de socios
+function limpiarFormulario() {
+
+    document.getElementById("documento").value = "";
+    document.getElementById("nombreSocio").value = "";
+    document.getElementById("localidadSocio").value = "";
+    document.getElementById("edadSocio").value = "";
+}
+
+
+
+/*
+let notaMatematica = 0;
+let notaLiteratura = 0;
+let notaGeografia = 0;
+let nombreAlumno = "";
+let cantNotasMatematica = 0;
+let cantNotasGeografica = 0;
+let cantNotasLiteratura = 0;
+let opcion = 0;
+*/
 
 
 /*
