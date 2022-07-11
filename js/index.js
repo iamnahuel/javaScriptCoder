10da587017b60033f649685e0a3311fa16591ce0
Facundo <catuno_rolon@hotmail.com>
2022-07-11 00:43:13 -0300

    DOM

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
 
 //declaro dos objetos de socio
-let socio1 = { nombre: "Facundo Alvarez", edad: 25, localidad: "Rolon", dni: 34475458 };
-let socio2 = { nombre: "Laura Perez", edad: 60, localidad: "Santa Rosa", dni: 14285470 };
-let socio3 = { nombre: "Juan Garcia", edad: 50, localidad: "Rolon", dni: 14245470 };
+let socio1 = { nombre: "Facundo Alvarez", edad: 25, localidad: "Rolon", dni: 34475458, tipoSocio: "Pleno" };
+let socio2 = { nombre: "Laura Perez", edad: 60, localidad: "Santa Rosa", dni: 14285470, tipoSocio: "Pleno" };
+let socio3 = { nombre: "Juan Garcia", edad: 50, localidad: "Rolon", dni: 14245470, tipoSocio: "Simple" };
 //declaramos array de socios, con dos socios harcodeados para que no este vacio
 const socios = [socio1, socio2, socio3];
 
 //funcion constructora para crear el objeto socio
-function Socio(dni, nombre, edad, localidad) {
+function Socio(dni, nombre, edad, localidad, tipoSocio) {
     this.dni = dni;
     this.nombre = nombre;
     this.edad = edad;
     this.localidad = localidad;
+    this.tipoSocio = this.tipoSocio;
 
 }
 
@@ -21,12 +22,15 @@ function agregarSocio() {
     let nombreSocio = document.getElementById("nombreSocioAgregar").value;
     let edadSocio = parseInt(document.getElementById("edadSocioAgregar").value);
     let localidadSocio = document.getElementById("localidadSocioAgregar").value;
-
-    //declaramos un variable donde cargar el objeto socio creado arriba
-    let socioTemporal = new Socio(dniSocio, nombreSocio, edadSocio, localidadSocio);
-    
-    //verificar que el dni no este cargado en el array previamente
+   //
+    var e = document.getElementById("tipoSocio");
+    var tipoSocio = e.options[e.selectedIndex].value;
    
+    //declaramos un variable donde cargar el objeto socio creado arriba
+    let socioTemporal = new Socio(dniSocio, nombreSocio, edadSocio, localidadSocio, tipoSocio);
+
+    //verificar que el dni no este cargado en el array previamente
+
     const resultado = socios.find((el) => el.dni === dniSocio);
     if (resultado != null) {
         alert("Ya existe un socio con este numero de documento");
@@ -38,6 +42,7 @@ function agregarSocio() {
             //cargamos el objetos socioTemporal al array de socios
             socios.push(socioTemporal);
             alert("Se agrego a " + nombreSocio + " exitosamente");
+            limpiarFormulario();
         }
     }
     return socios;
@@ -48,20 +53,12 @@ function agregarElementos() {
     var lista = document.getElementById("ulListado");
     socios.forEach(function (data, index) {
         var linew = document.createElement("li");
-        var contenido = document.createTextNode("DNI: " + data.dni + ' | Nombre: ' + data.nombre + " | Edad: " + data.edad + " | Localidad: " + data.localidad);
-        lista.appendChild(linew);
+        var contenido = document.createTextNode("DNI: " + data.dni + ' | Nombre: ' + data.nombre + " | Edad: " + data.edad + " | Localidad: " + data.localidad + " | Tipo Socio: " + data.tipoSocio);
         linew.appendChild(contenido);
-
+        lista.appendChild(linew);
     })
 }
 
-//mostrar array por consola
-function listarSocios() {
-    //metodo forof para poder listar el contenido de un array
-    for (const socio of socios) {
-        console.log(socio.dni, "|", socio.nombre, "|", socio.edad, "|", socio.localidad);
-    }
-}
 
 //buscar socio por dni o nombre
 function buscarSocio() {
@@ -87,7 +84,8 @@ function eliminarSocio() {
         alert("No se ha cargado ningun socio");
     } else {
         socios.splice(position, 1);
-        alert("Se elimino a "+ bandera + " exitosamente");
+        alert("Se elimino a " + bandera + " exitosamente");
+        limpiarFormulario();
     }
     return socios;
 }
@@ -98,86 +96,10 @@ function limpiarFormulario() {
     document.getElementById("nombreSocio").value = "";
     document.getElementById("localidadSocio").value = "";
     document.getElementById("edadSocio").value = "";
-}
-
-
-
-/*
-let notaMatematica = 0;
-let notaLiteratura = 0;
-let notaGeografia = 0;
-let nombreAlumno = "";
-let cantNotasMatematica = 0;
-let cantNotasGeografica = 0;
-let cantNotasLiteratura = 0;
-let opcion = 0;
-*/
-
-
-/*
-function ingresarAlumno() {
-    nombreAlumno = prompt("Nombre del Alumno: ");
-    opcion = prompt("Materia a calificar: \n 1- Matematica \n 2- Literatura \n 3- Geografia \n 4-Salir")
-    while (opcion < 4) {
-
-        switch (opcion) {
-            case "1":
-                cantNotasMatematica = parseInt(prompt("Cuantas notas quiere ingresar?"));
-                for (i = 1; i <= cantNotasMatematica; i++) {
-                    let nota = parseInt(prompt("Ingrese la nota de Matematicas N° " + i + " de " + nombreAlumno));
-                    notaMatematica = notaMatematica + nota;
-                }
-                opcion = prompt("Desea Calificar otra materia: \n 1- Matematica \n 2- Literatura \n 3- Geografia \n 4-Salir")
-                break;
-            case "2":
-                cantNotasLiteratura = parseInt(prompt("Cuantas notas quiere ingresar?"));
-                for (i = 1; i <= cantNotasLiteratura; i++) {
-                    let nota = parseInt(prompt("Ingrese la nota de Literatura N° " + i + " de " + nombreAlumno));
-                    notaLiteratura = notaLiteratura + nota;
-                }
-                opcion = prompt("Desea Calificar otra materia: \n 1- Matematica \n 2- Literatura \n 3- Geografia \n 4-Salir")
-                break;
-            case "3":
-                cantNotasGeografica = parseInt(prompt("Cuantas notas quiere ingresar?"));
-                for (i = 1; i <= cantNotasGeografica; i++) {
-                    let nota = parseInt(prompt("Ingrese la nota de Geografia N° " + i + " de " + nombreAlumno));
-                    notaGeografia = notaGeografia + nota;
-                }
-                opcion = prompt("Desea Calificar otra materia: \n 1- Matematica \n 2- Literatura \n 3- Geografia \n 4-Salir")
-                break;
-            case "4":
-                break;
-            default:
-                alert("Opcion incorrect");
-                opcion = 4;
-                break;
-        }
-    }
+    document.getElementById("nombreSocioAgregar").value = "";
+    document.getElementById("dniSocioAgregar").value = "";
+    document.getElementById("localidadSocioAgregar").value = "";
+    document.getElementById("edadSocioAgregar").value = "";
 
 }
 
-function calcularPromedio(sumaNotas, cantidadNotas) {
-    if (cantidadNotas == 0) {
-        alert("No hay notas de Matematica cargadas");
-    } else {
-        let promedio = sumaNotas / cantidadNotas;
-        alert(nombreAlumno + " tiene un promedio en Matematicas de: " + promedio.toFixed(2));
-    }
-}
-function matepromedioMatica() {
-    sumaNotas = notaMatematica;
-    cantidadNotas = cantNotasMatematica;
-    calcularPromedio(sumaNotas, cantidadNotas);
-}
-
-function promedioGeografia() {
-    sumaNotas = notaGeografia;
-    cantidadNotas = cantNotasGeografica;
-    calcularPromedio(sumaNotas, cantidadNotas);
-}
-function promedioLiteratura() {
-    sumaNotas = notaLiteratura;
-    cantidadNotas = cantNotasLiteratura;
-    calcularPromedio(sumaNotas, cantidadNotas);
-}
-*/
